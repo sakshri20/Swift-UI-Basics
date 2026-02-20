4f8b6ac27947bfdad015dbf5c869e16e3bbb51ac
Sakshi Shrivastava <sak.20.shrivastava@gmail.com>
2026-02-20 15:40:48 -0800

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Sakshi Shrivastava on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
